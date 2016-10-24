fc25ddc80ecfca73b6484518027d7f690b101aeb
Shaojung <shaojung@gmail.com>
2016-10-24 16:25:58 +0800

    ProgressBar (use multi thread)

--- app/src/main/java/com/example/user/yvd102405/MainActivity.java
+++ app/src/main/java/com/example/user/yvd102405/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.user.yvd102405;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
@@ -12,11 +14,13 @@ public class MainActivity extends AppCompatActivity {
     CheckBox chk;
     RadioButton rb;
     RadioGroup rg;
+    ProgressBar pb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         chk = (CheckBox) findViewById(R.id.checkBox);
+        pb = (ProgressBar) findViewById(R.id.progressBar);
         chk.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -47,6 +51,28 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+    }
+    public void click1(View v)
+    {
+        pb.setVisibility(View.VISIBLE);
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        pb.setVisibility(View.INVISIBLE);
+                    }
+                });
+            }
+        }.start();
 
     }
 }
