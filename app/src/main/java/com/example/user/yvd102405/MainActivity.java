88e89df52ee95b2137fcb5a60a30ef98d6084fc5
Shaojung <shaojung@gmail.com>
2016-10-24 15:46:12 +0800

    radio group

--- app/src/main/java/com/example/user/yvd102405/MainActivity.java
+++ app/src/main/java/com/example/user/yvd102405/MainActivity.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
     CheckBox chk;
+    RadioButton rb;
+    RadioGroup rg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,8 +23,30 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("MYLOG", "chk:" + isChecked);
             }
         });
-
         // chk.isChecked()
+
+        rb = (RadioButton) findViewById(R.id.radioButton);
+        rb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+            }
+        });
+
+        rg = (RadioGroup) findViewById(R.id.radioGroup);
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.radioButton)
+                {
+                    Log.d("MYLOG", "select 1111");
+                }
+                else if (checkedId == R.id.radioButton2)
+                {
+                    Log.d("MYLOG", "select 2222");
+                }
+            }
+        });
 
     }
 }
